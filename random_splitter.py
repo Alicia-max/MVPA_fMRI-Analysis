c9bfc0f48d100d8ca81cf48f58fb972142831f44
ogutsevda <sevda.ogut@epfl.ch>
2022-12-09 17:21:06 +0100

    SpaceNet run 5fold CV

--- random_splitter.py
+++ random_splitter.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split, LeaveOneGroupOut
 
 if __name__ == '__main__':
+
     X = np.linspace(0, 1, 100)
     y = 2 * X
 
@@ -17,6 +18,6 @@ if __name__ == '__main__':
         train_x_split = X[train_ind_split]
         train_y_split = y[train_ind_split]
 
-        test_x_split = X[val_ind_split]
-        test_y_split = y[val_ind_split]
+        val_x_split = X[val_ind_split]
+        val_y_split = y[val_ind_split]
         print("sdfsdf")
